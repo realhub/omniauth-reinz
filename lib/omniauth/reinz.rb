e0f35e8b6b26fc3d4dd830618f957224960301ef
Jacques Greeff <jacques@realhub.com.au>
2022-05-09 15:55:51 +1000

    first commit

---
+++ lib/omniauth/reinz.rb
@@ -0,0 +1,8 @@
+require "omniauth/reinz/version"
+
+module Omniauth
+  module Reinz
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/reinz/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Reinz
+    VERSION = "0.1.0"
+  end
+end
